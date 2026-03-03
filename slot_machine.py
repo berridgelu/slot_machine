b4349b42f6279ecd9927cd652a1aa65b9894e30f
berridgelu <berridgelu@wgc.school.nz>
2026-03-04 11:19:54 +1300

    Add files via upload

--- slot_machine.py
+++ slot_machine.py
@@ -4,19 +4,27 @@ print("SLOT MACHINE SIMULATOR")
 # Import random
 import random
 
-print("Hi Lucy")
+# Prompt user to play or recieve further game information until they give a valid response
+while True:
+    # Give user options for the pay table, paylines and to play
+    instructions = int(input(f"""
+Press 1 for the pay table
+Press 2 for paylines
+Press 3 to spin
+> """))
 
-# Print pay table
-print("""
+    if instructions == 1:
+        # Print pay table
+        print("""
 Pay table:
 5 of a kind - 10,000 credits
 4 of a kind - 1,000 credits
 3 of a kind - 100 credits
+""")
 
-Paylines are counted from left to right""")
-
-# Print paylines
-print("""Paylines:
+    elif instructions == 2:
+        # Print paylines
+        print("""Paylines:
 
 * * * * * - 1
 * * * * * - 2
@@ -30,45 +38,77 @@ print("""Paylines:
   *   *
 *       * - 5
 
-""")
+Paylines are counted from left to right
+              """) 
+
+    elif instructions == 3:
+        break
+
+    else:
+        print("Please enter a valid number")
 
 # Create symbol list
 symbols = ["❤", "⭐", "$", "🍒", "🍎"]
 
-# Create slot machine in the form of a list
-slot_machine = []
+# Counter to control if the loop repeats
+repeat = True
 
-# Create 3 rows and 5 columns with one random symbol on each row
-# Print out each row
-for a in range(3):
-    slot_machine.append([])
-    for b in range(5):
-        slot_machine[a].append(random.choice(symbols))
-    print(' '.join(slot_machine[a]))
-    print('')
+while repeat == True:
+    print("spinning...")
+    print("")
 
-# Creates 2d payline list
-paylines = []
+    # Create slot machine in the form of a list
+    slot_machine = []
 
-# Adds paylines 1-3 by looping through slot_machine
-for row in slot_machine:
-    paylines.append(row)
-    
-# Manually creates paylines 4-5
-paylines.append([slot_machine[0][0], slot_machine[1][1], slot_machine[2][2], slot_machine[1][3], slot_machine[0][4]])
-paylines.append([slot_machine[2][0], slot_machine[1][1], slot_machine[0][2], slot_machine[1][3], slot_machine[2][4]])
+    # Create 3 rows and 5 columns with one random symbol on each row
+    # Print out each row
+    for a in range(3):
+        slot_machine.append([])
+        for b in range(5):
+            slot_machine[a].append(random.choice(symbols))
+        print("\t".join(slot_machine[a]))
+        print('')
 
-# Checks and prints winning conditions
-for a in range(5):
-    # Consec  is a counter that checks for consecutive symbols in a payline
-    consec = 0
-    for b in range(5):
-        if paylines[a][b] == paylines[a][0]:
-            consec += 1
+    # Creates 2d payline list
+    paylines = []
+
+    # Adds paylines 1-3 by looping through slot_machine
+    for row in slot_machine:
+        paylines.append(row)
+        
+    # Manually creates paylines 4-5
+    paylines.append([slot_machine[0][0], slot_machine[1][1], slot_machine[2][2], slot_machine[1][3], slot_machine[0][4]])
+    paylines.append([slot_machine[2][0], slot_machine[1][1], slot_machine[0][2], slot_machine[1][3], slot_machine[2][4]])
+
+    # Checks and prints winning conditions
+    for a in range(5):
+        # Consec  is a counter that checks for consecutive symbols in a payline
+        consec = 0
+        for b in range(5):
+            if paylines[a][b] == paylines[a][0]:
+                consec += 1
+            else:
+                break
+        if consec >= 3:
+            print(f"Payline {a+1} wins {10**(consec-1)} credits")
         else:
-            break
-    if consec >= 3:
-        print(f"Payline {a+1} wins {10**(consec-1)} credits")
-    else:
-        print(f"Payline {a+1} wins nothing")
+            print(f"Payline {a+1} wins nothing")
+    
+    # Ask user if they want to play again until they enter a valid response
+    while True:
+        # Asks user if they want to spin again
+        repeat = int(input("""
+Press 1 to spin again
+Press 2 to finish
+> """))
+        
+        # Checks users choice
+        if repeat == 2:
+            repeat = False
+            
+        else:
+            print("Please enter a valid response")
+            pass
+
+        break
 
